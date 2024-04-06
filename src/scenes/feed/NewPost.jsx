a408d09351fc9a12d27998d0e674ee2907979b49
Lara Scremin <lscreminmendes@gmail.com>
2024-04-06 19:01:40 -0300

    new posts

--- src/scenes/feed/NewPost.jsx
+++ src/scenes/feed/NewPost.jsx
@@ -1,23 +1,27 @@
 import "./NewPost.css";
 import NewPostPic from "../../assets/profile.png";
+import UploadIcon from "../../assets/upload-icon.svg";
+
+import Forms from "../login/Forms";
 
 function NewPost() {
   return (
     <div className="new-post">
       <div className="new-post-top">
         <img src={NewPostPic} />
-        <form action="/" method="POST">
+        <form>
           <input
-            type="text"
-            id="newPost"
-            name="newPost"
             className="input-post"
-          ></input>
+            placeholder="Compartilhe algo legal..."
+          />
         </form>
       </div>
       <div className="new-post-bottom">
-        <button>ADICIONAR IMAGEM</button>
-        <button>POSTAR</button>
+        <button className="adicionar">
+          <img src={UploadIcon} />
+          Adicionar Imagem
+        </button>
+        <button className="postar">POSTAR</button>
       </div>
     </div>
   );
